4e0740db1244c92d4ea5d9a29781abc7d2232de2
SHUMKARBEK <menshumkarbek@gmail.com>
2026-08-27 13:39:12 +0600

    Fix sitemap domain to production URL

--- shop/sitemaps.py
+++ shop/sitemaps.py
@@ -4,9 +4,16 @@ from django.urls import reverse
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = 'daily'
+    protocol = 'https'
 
     def items(self):
         return ['shop:home', 'shop:product_list', 'shop:about_us', 'shop:contact_us']
 
     def location(self, item):
         return reverse(item)
+
+    def get_urls(self, page=1, site=None, protocol=None):
+        urls = super().get_urls(page, site, protocol=self.protocol)
+        for url_info in urls:
+            url_info['location'] = url_info['location'].replace('example.com', 'techvault-c6us.onrender.com')
+        return urls
